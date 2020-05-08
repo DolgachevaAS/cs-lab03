94214ba0c651b05b8b46a26ac467783fdebf16d5
Bob (DolgachevaAS) <bob@DolgachevaAS.com>
2020-05-08 11:23:17 +0300

    code: замена ввода данных на вызов read_input

--- main.cpp
+++ main.cpp
@@ -6,6 +6,11 @@
 
 using namespace std;
 
+struct Input {
+    vector<double> numbers;
+    size_t bin_count;
+};
+
 vector<double> input_numbers(istream& in, size_t count)
 {
     vector <double> result(count);
@@ -16,19 +21,41 @@ vector<double> input_numbers(istream& in, size_t count)
     return result;
 
 }
-vector<size_t> make_histogram(const vector <double> &numbers,double &max, double &min, size_t &bin_count)
+
+Input
+read_input(istream& in) {
+    Input data;
+
+    cerr << "Enter number count: ";
+    size_t number_count;
+    in >> number_count;
+
+    cerr << "Enter numbers: ";
+    data.numbers = input_numbers(in, number_count);
+
+     cerr<<"bin_count=";
+     size_t bin_count;
+     in>> bin_count;
+  double max=0, min=0;
+ find_minmax(data.numbers,max,min);
+    return data;
+}
+
+
+vector<size_t> make_histogram (size_t count,const vector<double>& numbers)
+{ double min=0;
+double max=0;
+find_minmax(numbers,min,max);
+vector<size_t> bins(count);
+for (double number : numbers) {
+size_t bin = (size_t)((number - min) / (max - min) * count);
+if (bin == count)
 {
-    vector<size_t>bins(bin_count,0);
-    for(double x : numbers)
-    {
-        size_t bin_index=(size_t)((x-min)*bin_count/(max-min));
-        if (bin_index==bin_count)
-        {
-            bin_index=bin_index-1;
-        }
-        bins[bin_index]++;
-    }
-    return bins;
+bin--;
+}
+bins[bin]++;
+}
+return bins;
 }
 
 
@@ -39,15 +66,12 @@ int main()
     double baseline;
     string text;
     size_t number_count, bin_count;
-    double max=0, min=0;
-    cerr<<"number_count=";
-    cin>>number_count;
-    cerr<<"Enter number count: "<<"\n";
-    const auto numbers = input_numbers(cin,number_count);
-    cerr<<"bin_count=";
-    cin>> bin_count;
-    find_minmax(numbers,max,min);
-    const auto bins=make_histogram(numbers,max,min,bin_count);
-    show_histogram_svg(bins,number_count);
-    return 0;
-}
+
+    Input data=read_input(cin);
+
+    const auto bins=make_histogram(data.bin_count,data.numbers);
+    show_histogram_svg(bins,data.bin_count);
+    return 0;}
+
+
+
